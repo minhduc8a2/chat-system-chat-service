2d5f9d730bb8bfb3e94c37bff0257534a6d95feb
Duke Lee <minhduc8a2.1@gmail.com>
2025-04-24 17:45:16 +0700

    remove sockjs

--- src/main/java/com/ducle/chat_service/controller/MessageController.java
+++ src/main/java/com/ducle/chat_service/controller/MessageController.java
@@ -4,6 +4,8 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
