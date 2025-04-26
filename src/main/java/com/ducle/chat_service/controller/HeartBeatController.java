70f23e3c85d7692fc7e0c23bb95fc4279c594a32
Duke Lee <minhduc8a2.1@gmail.com>
2025-04-26 16:47:32 +0700

    heartbeat working with client

--- src/main/java/com/ducle/chat_service/controller/HeartBeatController.java
+++ src/main/java/com/ducle/chat_service/controller/HeartBeatController.java
@@ -11,9 +11,11 @@ import org.springframework.stereotype.Controller;
 import com.ducle.chat_service.util.SessionUtils;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class HeartBeatController {
     @Value("${server.id}")
     private String serverId;
@@ -25,8 +27,12 @@ public class HeartBeatController {
 
     @MessageMapping("/heartbeat")
     public void receiveHeartbeat(Message<?> message) {
+
         Long userId = SessionUtils.getUserIdFromSession(message);
         String key = String.format(keyFormat, serverId, userId);
-        redisTemplate.opsForValue().set(key, "online", 30, TimeUnit.SECONDS);
+        log.info("Heartbeat----------------------------------");
+        log.info(key);
+
+        redisTemplate.opsForValue().set(key, "online", 15, TimeUnit.SECONDS);
     }
 }
