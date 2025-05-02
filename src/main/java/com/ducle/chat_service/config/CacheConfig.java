00f4fd3f44f2153bada25c07ef364061e7b42ed5
Duke Lee <minhduc8a2.1@gmail.com>
2025-05-02 11:35:38 +0700

    change cache config

--- src/main/java/com/ducle/chat_service/config/CacheConfig.java
+++ src/main/java/com/ducle/chat_service/config/CacheConfig.java
@@ -1,23 +1,19 @@
 package com.ducle.chat_service.config;
 
-import java.time.Duration;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
 public class CacheConfig {
     @Bean
-    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(15))
-                .disableCachingNullValues();
-
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(config)
-                .build();
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()));
     }
 }
--- src/main/java/com/ducle/chat_service/security/ChatRoomSecurity.java
+++ src/main/java/com/ducle/chat_service/security/ChatRoomSecurity.java
@@ -1,10 +1,8 @@
 package com.ducle.chat_service.security;
 
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import com.ducle.chat_service.exception.AccessDeniedException;
-import com.ducle.chat_service.util.SessionUtils;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
